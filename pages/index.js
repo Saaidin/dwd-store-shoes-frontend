1721db9da535ebf630a076d3ab763279867d2dfe
saaidin <dinme73@gmail.com>
2023-03-31 06:33:25 +0800

    construct images from cloud db

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,24 @@
 import HeroBanner from "@/components/HeroBanner"
 import ProductCard from "@/components/ProductCard"
 import Wrapper from "@/components/Wrapper"
-// import { fetchDataFromApi } from "@/utils/api"
+import { fetchDataFromApi } from "@/utils/api"
+// import { useEffect, useState } from "react"
 
-export default function Home() {
+export default function Home({ products }) {
+  // const [data, setData] = useState(null)
+  // useEffect(() => {
+  //   fetchProducts()
+  // }, [])
+
+  // const fetchProducts = async () => {
+  //   const { data } = await fetchDataFromApi("/api/products")
+  //   setData(data)
+  // }
+
   return (
     <main>
       <HeroBanner />
+      {/* <h1>{products?.data?.[3]?.attributes.name}</h1> */}
       <Wrapper>
         {/* heading and paragaph start */}
         <div className="text-center max-w-[800px] mx-auto my-[50px] md:my-[80px]">
@@ -23,9 +35,10 @@ export default function Home() {
 
         {/* products grid start */}
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 my-14 px-5 md:px-0">
-          {/* {products?.data?.map((product) => (
+          {products?.data?.map((product) => (
             <ProductCard key={product?.id} data={product} />
-          ))} */}
+          ))}
+          {/* <ProductCard />
           <ProductCard />
           <ProductCard />
           <ProductCard />
@@ -33,8 +46,7 @@ export default function Home() {
           <ProductCard />
           <ProductCard />
           <ProductCard />
-          <ProductCard />
-          <ProductCard />
+          <ProductCard /> */}
         </div>
         {/* products grid end */}
       </Wrapper>
@@ -42,10 +54,10 @@ export default function Home() {
   )
 }
 
-// export async function getStaticProps() {
-//   const products = await fetchDataFromApi("/api/products?populate=*")
+export async function getStaticProps() {
+  const products = await fetchDataFromApi("/api/products?populate=*")
 
-//   return {
-//     props: { products },
-//   }
-// }
+  return {
+    props: { products },
+  }
+}
