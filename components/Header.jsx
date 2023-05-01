4fdbc392f7328b7dd6a32c4b05747445539769ea
saaidin <dinme73@gmail.com>
2023-05-02 03:53:52 +0800

    header section - done

--- components/Header.jsx
+++ components/Header.jsx
@@ -5,7 +5,8 @@ import Link from "next/link"
 import Menu from "./Menu"
 import MenuMobile from "./MenuMobile"
 
-import { IoMdHeartEmpty } from "react-icons/io"
+import { MdSearch } from "react-icons/md"
+import { MdPermIdentity } from "react-icons/md"
 import { BsCart } from "react-icons/bs"
 import { BiMenuAltRight } from "react-icons/bi"
 import { VscChromeClose } from "react-icons/vsc"
@@ -54,32 +55,16 @@ const Header = () => {
     <header
       className={`w-full h-[50px] md:h-[80px] bg-white flex items-center justify-between z-20 sticky top-0 transition-transform duration-300 ${show}`}
     >
-      <Wrapper className="h-[60px] flex justify-between items-center">
-        <Link href="/">
-          <img src="/logo.svg" alt="logo" className="w-[40px] md:w-[60px]" />
-        </Link>
-        <Menu
-          showCatMenu={showCatMenu}
-          setShowCatMenu={setShowCatMenu}
-          categories={categories}
-        />
-
-        {mobileMenu && (
-          <MenuMobile
-            showCatMenu={showCatMenu}
-            setShowCatMenu={setShowCatMenu}
-            setMobileMenu={setMobileMenu}
-            categories={categories}
-          />
-        )}
+      <Wrapper className="h-[60px] flex justify-between items-center text-secondary-500">
+        <Link href="/">ECOMMER</Link>
 
         <div className="flex items-center gap-2 text-black">
           {/* Icon start */}
           <div className="w-8 md:w-12 h-8 md:h-12 rounded-full flex justify-center items-center hover:bg-black/[0.05] cursor-pointer relative">
-            <IoMdHeartEmpty className="text-[19px] md:text-[24px]" />
-            <div className="h-[14px] md:h-[18px] min-w-[14px] md:min-w-[18px] rounded-full bg-red-600 absolute top-1 left-5 md:left-7 text-white text-[10px] md:text-[12px] flex justify-center items-center px-[2px] md:px-[5px]">
-              51
-            </div>
+            <MdSearch className="text-[19px] md:text-[24px]" />
+          </div>
+          <div className="w-8 md:w-12 h-8 md:h-12 rounded-full flex justify-center items-center hover:bg-black/[0.05] cursor-pointer relative">
+            <MdPermIdentity className="text-[19px] md:text-[24px]" />
           </div>
           {/* Icon end */}
 
