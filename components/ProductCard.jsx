02ddce8cecfa2518cb9fbf26e284cb3180e76230
saaidin <dinme73@gmail.com>
2023-05-06 02:20:37 +0800

    last checking

--- components/ProductCard.jsx
+++ components/ProductCard.jsx
@@ -19,12 +19,12 @@ const ProductCard = ({ data: { attributes: p, id } }) => {
       <div className="p-4 text-black/[0.9]">
         <h2 className="text-lg font-medium">{p.name}</h2>
         <div className="flex items-center text-black/[0.5]">
-          <p className="mr-2 text-lg font-semibold">&#8377;{p.price}</p>
+          <p className="mr-2 text-lg font-semibold">RM{p.price}</p>
 
           {p.original_price && (
             <>
               <p className="text-base  font-medium line-through">
-                &#8377;{p.original_price}
+                RM{p.original_price}
               </p>
               <p className="ml-auto text-base font-medium text-green-500">
                 {getDiscountedPricePercentage(p.original_price, p.price)}% off
